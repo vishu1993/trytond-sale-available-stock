75a8380f3ab97f106fe669b7c024277637b9f70e
Prakash Pandey <prakash.pandey@openlabs.co.in>
2015-02-17 18:56:15 +0530

    Pick long description from README.md

--- setup.py
+++ setup.py
@@ -107,6 +107,7 @@ setup(
     author="Openlabs Technologies and Consulting (P) Ltd.",
     author_email='info@openlabs.co.in',
     url='http://www.openlabs.co.in/',
+    long_description=open('README.md').read(),
     package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
         'trytond.modules.%s' % MODULE,
